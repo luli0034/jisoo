73180b7d5c8f28340bc88e5850a4550aa7fd6b79
LuliLee <lulilee@kkcompany.com>
2025-02-17 13:24:05 +0800

    chore: add test case for result selector

--- tests/test_state.py
+++ tests/test_state.py
@@ -154,7 +154,7 @@ def test_task():
         resource="arn:aws:states:::lambda:invoke",
         parameters={"FunctionName": "my-function"},
         result_path="$.result",
-        result_selector={"foo": "$.bar"},
+        result_selector={"foo": "$.bar", "baz.$": "$.qux"},
     )
 
     state_dict = task_state.to_dict()
@@ -164,7 +164,8 @@ def test_task():
     assert state_dict["Resource"] == "arn:aws:states:::lambda:invoke"
     assert state_dict["Parameters"]["FunctionName"] == "my-function"
     assert state_dict["ResultPath"] == "$.result"
-    assert state_dict["ResultSelector"] == {"foo.$": "$.bar"}
+    assert state_dict["ResultSelector"]["foo.$"] == "$.bar"
+    assert state_dict["ResultSelector"]["baz.$"] == "$.qux"
 
 
 def test_task_with_error_control():
